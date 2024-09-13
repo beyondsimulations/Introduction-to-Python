a6002a795ae9ffd1587dfc9bb345631fe769a4fd
Tobias  Vlćek <vlcek@beyondsimulations.com>
2024-09-13 07:37:48 +0200

    fixed website

---
+++ part-01/solution/1_string_operations.py
@@ -0,0 +1,16 @@
+# a) Concatenation
+text = "Coffee" + " is fun!"
+print(text)
+
+# b) Length
+print(len(text))
+
+# c) Indexing
+print(text[0])
+print(text[len(text)-1])
+
+# d) Slicing
+print(text[0:3])
+
+# e) Repetition
+print("Coffee" * 3)
