767af3e5969b3dfe99f7ce4f171bc98033c7d245
Tobias Vlcek <vlcek@beyondsimulations.com>
2026-07-12 14:45:31 +0200

    chore: retire pre-overhaul assignments and solutions into untracked archive

--- solutions-lectures/lec_02/secret_message.py
+++
@@ -1,8 +0,0 @@
-# Slice the following message to create the described output
-message = "y6S0-teru89d23e'.n*ut"
-# Your code here
-
-message = message[2::3]
-print(message)
-
-output = "Student"
--- solutions-lectures/lec_08/lec_07_numpy1.py
+++
@@ -1,6 +0,0 @@
-# TODO: Create an array with 10 evenly spaced numbers over the interval from 0 to 73.
-
-import numpy as np
-# YOUR CODE HERE
-
-print(np.linspace(0, 73, 10))
